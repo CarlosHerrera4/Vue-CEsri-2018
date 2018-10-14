fe525aa059c302f02664759f8d2c000cb058eeb0
MundoGister <mundogister@gmail.com>
2018-10-14 18:38:46 +0200

    feat: show cards list events

--- js/index.js
+++ js/index.js
@@ -59,17 +59,15 @@ require([
         "<div class='card' style='width: 18rem;'>",
             "<img class='card-img-top' src='...' alt='Card image cap'>",
             "<div class='card-body'>",
-                "<h5 class='card-title'>Card title</h5>",
+                "<h5 class='card-title'>{{ event.name }}</h5>",
                " <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>",
                 "<a href='#' class='btn btn-primary'>Go somewhere</a>",
             "</div>",
         "</div>"
             
         ].join(""),
-        methods: {
-            reset: function () {
-                console.log("hola")
-            }
+        props: {
+            event: Object
         }
     });
 
@@ -77,7 +75,10 @@ require([
 
     view.when(function () {
         const _info = new Vue({
-            el: '#container'
+            el: '#container',
+            data: {
+                events: data
+            }
         });
 
         const info = new Vue({
