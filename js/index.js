2e9de5230e8752b8cb378e7770f1c11d3a230dc1
MundoGister <mundogister@gmail.com>
2018-10-16 09:25:16 +0200

    feat: add example data

--- js/index.js
+++ js/index.js
@@ -45,8 +45,29 @@ require([
                 "<h5 class='card-title'>{{ event.name }}</h5>",
                " <p class='card-text'>{{ event.description }}</p>",
                 "<a href='#' v-on:click='goTo' class='btn btn-primary'>Ir al sitio</a>",
+                // "<a href='#' v-on:click='showModal' data-toggle='modal' data-target='hola' class='btn btn-primary'>Más información</a>",
             "</div>",
         "</div>"
+
+        // "<div class='modal fade' v-bind:id='{{ event.id }}' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>",
+        //     "<div class='modal-dialog' role='document'>",
+        //         "<div class='modal-content'>",
+        //         "<div class='modal-header'>",
+        //             "<h5 class='modal-title' id='exampleModalLabel'>Modal title</h5>",
+        //             "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>",
+        //             "<span aria-hidden='true'>&times;</span>",
+        //             "</button>",
+        //         "</div>",
+        //         "<div class='modal-body'>",
+        //         "</div>",
+        //         "<div class='modal-footer'>",
+        //             "<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>",
+        //             "<button type='button' class='btn btn-primary'>Save changes</button>",
+        //         "</div>",
+        //         "</div>",
+        //     "</div>",
+        // "</div>",
+
         ].join(""),
         props: {
             event: Object
@@ -61,10 +82,14 @@ require([
                 var camera = view.camera.clone();
                 camera.set(newCamera);
                 view.goTo(camera);
-            }   
+            },
+            
+            showCard: function () {
+
+            },
+
         }
     });
-
    
 
     view.when(function () {
@@ -75,7 +100,6 @@ require([
                 }
             })]
         });
-
 
         const _info = new Vue({
             el: '#container',
